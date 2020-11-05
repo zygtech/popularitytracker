dfeb5d7265a156752eef12fe4f2cd4a65f600f44
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-11-05 22:01:59 +0100

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -7,16 +7,17 @@ import populartimes, datetime, sys
 def saveid(placeid):
     api = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
     content = populartimes.get_id(api,placeid)
-    if (sys.argv[1]=="init"):
-        f = open("names/" + placeid + ".txt", "w")
-        f.write(content["name"] + ' ' + content["address"])
-    else:
+    if (len(sys.argv) == 0):
         f = open("places/" + placeid + ".txt", "a")
         d = datetime.datetime.now()
         try:
             f.write(d.strftime("%Y-%m-%d %H") + ": " + str(content["current_popularity"]) + "\n")
         except:
             f.write(d.strftime("%Y-%m-%d %H") + ": N\n")
+    else:
+        if (sys.argv[1]=="init"):
+            f = open("names/" + placeid + ".txt", "w")
+            f.write(content["name"] + ' ' + content["address"])
     f.close()
 
 def main():
